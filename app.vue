f5b66663ec25d53356c2ff4d212219ecf0877856
Vivian Hayes <vivian.xw7@gmail.com>
2024-04-10 14:20:54 -0500

    first commit

---
+++ app.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="layout">
+    <NuxtPage />
+  </div>
+</template>
+
+<style lang="scss">
+@use "@/assets/styles/global.scss";
+
+.layout {
+  width: 620px;
+  margin: 50px auto 50px;
+}
+</style>
